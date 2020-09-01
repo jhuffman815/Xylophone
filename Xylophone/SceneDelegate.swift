c1e2d359eee86e59226b30317159830b0b3caffb
Jewell <jhuffman815@gmail.com>
2020-09-01 16:34:30 -0400

    First commit

--- Xylophone/SceneDelegate.swift
+++ Xylophone/SceneDelegate.swift
@@ -2,8 +2,7 @@
 //  SceneDelegate.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Jewell Hufmnan 28/06/2019.
 //
 
 import UIKit
